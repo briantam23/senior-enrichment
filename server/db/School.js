072f61a9fe98934a65a2e9975fc403ad4871d272
briantam23 <briantam23@gmail.com>
2019-07-13 07:34:00 -0400

    Fixed security vulnerability

--- server/db/School.js
+++ server/db/School.js
@@ -1,31 +1,37 @@
 const conn = require('./conn');
 
-const School = conn.define('school', {
-    name: {
-        type: conn.Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: { notEmpty: true }
+const School = conn.define('school', 
+    {
+        name: {
+            type: conn.Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: { notEmpty: true }
+        },
+        description: {
+            type: conn.Sequelize.STRING,
+            allowNull: true,
+            unique: false
+        },
+        address: {
+            type: conn.Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: { notEmpty: true }
+        }
     },
-    description: {
-        type: conn.Sequelize.STRING,
-        allowNull: true,
-        unique: false
-    },
-    address: {
-        type: conn.Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: { notEmpty: true }
+    {
+        hooks: {
+            beforeCreate: (school, options) => schoolHook(school),
+            beforeUpdate: (school, option) => schoolHook(school)
+        }
     }
-})
+)
 
 const schoolHook = school => {
     const { name } = school;
     school.name = name.charAt(0).toUpperCase() + name.slice(1);
 }
 
-School.beforeCreate((school, options) => schoolHook(school));
-School.beforeUpdate((school, option) => schoolHook(school))
 
 module.exports = School;
--- server/db/Student.js
+++ server/db/Student.js
@@ -1,26 +1,34 @@
 const conn = require('./conn');
 
-const Student = conn.define('student', {
-    lastName: {
-        type: conn.Sequelize.STRING,
-        allowNull: false,
-        unique: false,
-        validate: { notEmpty: true }
+const Student = conn.define('student', 
+    {
+        lastName: {
+            type: conn.Sequelize.STRING,
+            allowNull: false,
+            unique: false,
+            validate: { notEmpty: true }
+        },
+        firstName: {
+            type: conn.Sequelize.STRING,
+            allowNull: false,
+            unique: false,
+            validate: { notEmpty: true }
+        },
+        GPA: {
+            type: conn.Sequelize.DECIMAL(2, 1),
+            allowNull: false,
+            unique: false,
+            validate: { min: 0.0, max: 4.0 },
+            defaultValue: 3.3
+        }
     },
-    firstName: {
-        type: conn.Sequelize.STRING,
-        allowNull: false,
-        unique: false,
-        validate: { notEmpty: true }
-    },
-    GPA: {
-        type: conn.Sequelize.DECIMAL(2, 1),
-        allowNull: false,
-        unique: false,
-        validate: { min: 0.0, max: 4.0 },
-        defaultValue: 3.3
+    {
+        hooks: {
+            beforeCreate: (student, options) => studentHook(student),
+            beforeUpdate: (student, options) => studentHook(student)
+        }
     }
-})
+)
 
 const studentHook = student => {
     const { lastName, firstName } = student;
@@ -28,7 +36,5 @@ const studentHook = student => {
     student.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
 }
 
-Student.beforeCreate((student, options) => studentHook(student));
-Student.beforeUpdate((student, options) => studentHook(student));
 
 module.exports = Student;
